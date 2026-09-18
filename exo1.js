7b63c8c049c3768457c7f0948766c898dac08251
Sandy-1805 <sandyharintsoa@gmail.com>
2026-09-18 13:46:10 +0300

    exercice 1

---
+++ exo1.js
@@ -0,0 +1,26 @@
+let valeur = [3, -1, 0, 4, -2, 5, -3];
+
+function echanger(tab, index1, index2) {
+    let temp = tab[index1];
+    tab[index1] = tab[index2];
+    tab[index2] = temp;
+}
+
+function NegatifPositif(arr) {
+    let i = 0;
+    let j = arr.length - 1;
+    while (i < j) {
+        while (arr[i] < 0 && i < j) {
+            i++;
+        }
+        while (arr[j] > 0 && i < j) {
+            j--;
+        }
+        if (i < j) {
+            echanger(arr, i, j);
+        }
+    }
+    return arr;
+}
+
+console.log(NegatifPositif(valeur));
